e41f2e310e40769d0bd639aef02b74406a523652
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-05 09:19:19 -0300

    Ajustando para utilizar enum

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,22 +1,19 @@
 package view;
 
-import model.Aluno;
-import model.Pessoa;
-import model.Professor;
-import model.TecnicoAdministrativo;
+import model.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Professor professor = new Professor("222.222.222-02",
+        Funcionario professor = new Professor("222.222.222-02",
                 "Maria", LocalDate.now(), 3000,
                 "Mestrado");
-        TecnicoAdministrativo tecnico1 = new TecnicoAdministrativo(
+        Funcionario tecnico1 = new TecnicoAdministrativo(
                 "111.111.111-01", "Pedro", LocalDate.now(),
                 2000, "Laboratório de Química"
         );
-        TecnicoAdministrativo tecnico2 = new TecnicoAdministrativo(
+        Funcionario tecnico2 = new TecnicoAdministrativo(
                 "111.111.111-01", "José", LocalDate.now(),
                 2000, "Protocolo"
         );
